e286b8023e0392974ef713f3b0e0d48705548aae
zannabianca1997 <zannabianca199712@gmail.com>
2025-03-11 20:52:51 +0100

    Feat: inizialized quarkus project

---
+++ src/native-test/java/io/github/zannabianca1997/apelle/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package io.github.zannabianca1997.apelle;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
