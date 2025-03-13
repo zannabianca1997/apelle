4f0c2b13b7ffec7162790620d92a155bc20e59d3
zannabianca1997 <zannabianca199712@gmail.com>
2025-03-13 01:21:14 +0100

    Chore: import cleanup

--- src/main/java/io/github/zannabianca1997/apelle/user/exceptions/UserNotFoundException.java
+++ src/main/java/io/github/zannabianca1997/apelle/user/exceptions/UserNotFoundException.java
@@ -1,12 +1,5 @@
 package io.github.zannabianca1997.apelle.user.exceptions;
 
-import org.eclipse.microprofile.openapi.annotations.media.Content;
-import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
-import org.jboss.resteasy.reactive.RestResponse;
-
-import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.core.Response.Status;
-import jakarta.ws.rs.ext.ExceptionMapper;
 import lombok.Getter;
 
 @Getter
