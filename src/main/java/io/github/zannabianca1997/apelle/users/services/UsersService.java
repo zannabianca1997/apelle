21e68a04cc7bc375263f08951f0b64632a11510c
zannabianca1997 <zannabianca199712@gmail.com>
2025-03-22 12:22:46 +0100

    Docs: documented user service

--- src/main/java/io/github/zannabianca1997/apelle/users/services/UsersService.java
+++ src/main/java/io/github/zannabianca1997/apelle/users/services/UsersService.java
@@ -16,6 +16,13 @@ public class UsersService {
     @Inject
     private SecurityIdentity securityIdentity;
 
+    /**
+     * Crate a user
+     * 
+     * @param user The user to create
+     * @return The created user
+     * @throws UserAlreadyExistsException The user already exists
+     */
     public ApelleUser signup(ApelleUser user) throws UserAlreadyExistsException {
         if (ApelleUser.findByName(user.getName()) != null) {
             throw new UserAlreadyExistsException(user.getName());
@@ -24,6 +31,13 @@ public class UsersService {
         return user;
     }
 
+    /**
+     * Obtain a user by name
+     * 
+     * @param userName The user name
+     * @return The found user
+     * @throws UserNotFoundByNameException The user does not exists
+     */
     public ApelleUser get(String userName) throws UserNotFoundByNameException {
         ApelleUser user = ApelleUser.findByName(userName);
         if (user == null) {
@@ -32,6 +46,12 @@ public class UsersService {
         return user;
     }
 
+    /**
+     * Delete a user by name
+     * 
+     * @param userId The user name
+     * @throws UserNotFoundByNameException The user does not exists
+     */
     public void delete(String userName) throws UserNotFoundByNameException {
         ApelleUser user = ApelleUser.findByName(userName);
         if (user == null) {
@@ -40,6 +60,13 @@ public class UsersService {
         user.delete();
     }
 
+    /**
+     * Obtain a user by id
+     * 
+     * @param userId The user id
+     * @return The found user
+     * @throws UserNotFoundByNameException The user does not exists
+     */
     public ApelleUser get(UUID userId) throws UserNotFoundByIdException {
         ApelleUser user = ApelleUser.findById(userId);
         if (user == null) {
@@ -48,6 +75,12 @@ public class UsersService {
         return user;
     }
 
+    /**
+     * Delete a user by id
+     * 
+     * @param userId The user id
+     * @throws UserNotFoundByNameException The user does not exists
+     */
     public void delete(UUID userId) throws UserNotFoundByIdException {
         ApelleUser user = ApelleUser.findById(userId);
         if (user == null) {
@@ -56,12 +89,20 @@ public class UsersService {
         user.delete();
     }
 
+    /**
+     * Get the used currently logged in
+     * 
+     * @return The current user
+     */
     public ApelleUser getCurrent() {
         String name = securityIdentity.getPrincipal().getName();
         ApelleUser user = ApelleUser.findByName(name);
         return user;
     }
 
+    /**
+     * Delete the current user
+     */
     public void deleteCurrent() {
         String name = securityIdentity.getPrincipal().getName();
         ApelleUser.findByName(name).delete();
