12a7d14c801b40f4e9a8de97e7687ddda2e14af8
zannabianca1997 <zannabianca199712@gmail.com>
2025-03-25 01:27:43 +0100

    Feat: i18n

--- src/main/webui/src/routes/+layout.ts
+++ src/main/webui/src/routes/+layout.ts
@@ -1,2 +1,14 @@
-// This is a static site
+import { browser } from '$app/environment'
+import '$lib/i18n' // Import to initialize. Important :)
+import { locale, waitLocale } from 'svelte-i18n'
+import type { LayoutLoad } from './$types'
+
+export const load: LayoutLoad = async () => {
+    if (browser) {
+        locale.set(window.navigator.language)
+    }
+    await waitLocale()
+}
+
+// Create a static site
 export const ssr = false
