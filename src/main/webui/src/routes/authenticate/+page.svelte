8e1b95eb19dca1fec00071526b9471c9282feaaa
zannabianca1997 <zannabianca199712@gmail.com>
2025-03-27 00:00:56 +0100

    Feat: frontoffice

---
+++ src/main/webui/src/routes/authenticate/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import SigninForm from './SigninForm.svelte';
+	import SignupForm from './SignupForm.svelte';
+</script>
+
+<main>
+	<SigninForm />
+	<SignupForm />
+</main>
+
+<style lang="scss">
+	main {
+		display: flex;
+		gap: 37px;
+	}
+</style>
