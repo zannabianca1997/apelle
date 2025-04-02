800df5af602b3a80b1102e13151c280640c890b2
zannabianca1997 <zannabianca199712@gmail.com>
2025-04-02 20:11:43 +0200

    Feat: version regex

--- src/main/java/io/github/zannabianca1997/apelle/common/resources/VersionResource.java
+++ src/main/java/io/github/zannabianca1997/apelle/common/resources/VersionResource.java
@@ -4,7 +4,9 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 import jakarta.annotation.security.PermitAll;
 import jakarta.ws.rs.GET;
@@ -24,7 +26,7 @@ public class VersionResource {
     @PermitAll
     @Operation(summary = "Version of the server", description = "Return the version of the server")
     @APIResponse(responseCode = "200", description = "The version of the server", content = {
-            @Content(mediaType = "text/plain", example = "0.0.1")
+            @Content(mediaType = "text/plain", example = "0.0.1", schema = @Schema(type = SchemaType.STRING, pattern = "\\d+\\.\\d+\\.\\d+"))
     })
     public String version() {
         return version;
