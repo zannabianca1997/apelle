2bd04389a22e5d6021f57446e7b02dfe8d1ed13c
zannabianca1997 <zannabianca199712@gmail.com>
2025-04-05 11:29:38 +0200

    Feat: user represented as sub-resource

--- src/main/java/io/github/zannabianca1997/apelle/users/resources/UserResource.java
+++ src/main/java/io/github/zannabianca1997/apelle/users/resources/UserResource.java
@@ -4,42 +4,61 @@ import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
-import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import io.github.zannabianca1997.apelle.users.dtos.UserQueryDto;
 import io.github.zannabianca1997.apelle.users.mappers.UserMapper;
+import io.github.zannabianca1997.apelle.users.models.ApelleUser;
+import io.github.zannabianca1997.apelle.users.models.ApelleUserRole;
 import io.github.zannabianca1997.apelle.users.services.UsersService;
 import io.quarkus.security.Authenticated;
+import io.quarkus.security.PermissionChecker;
+import io.quarkus.security.PermissionsAllowed;
+import io.quarkus.security.identity.SecurityIdentity;
+import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
 
-@Path("/users/me")
-@Tag(name = "Users", description = "User management")
-public class UserMeResource {
+@RequestScoped
+@Authenticated
+public class UserResource {
 
     @Inject
     UserMapper userMapper;
     @Inject
     UsersService usersService;
 
+    ApelleUser user = null;
+
+    public UserResource of(ApelleUser user) {
+        this.user = user;
+        return this;
+    }
+
     @GET
-    @Authenticated
-    @Operation(summary = "Current user data", description = "Returns the data of the user that is currently logged in")
+    @Operation(summary = "The user data", description = "Returns the data of the user")
     @APIResponse(responseCode = "200", description = "The current user", content = {
             @Content(mediaType = "application/json", schema = @Schema(implementation = UserQueryDto.class))
     })
     public UserQueryDto get() {
-        return userMapper.toDto(usersService.getCurrent());
+        return userMapper.toDto(user);
     }
 
     @DELETE
-    @Authenticated
+    @PermissionsAllowed("user-delete")
     @Transactional
-    @Operation(summary = "Delete current user", description = "Delete the current user")
+    @Operation(summary = "Delete user", description = "Delete the user")
     @APIResponse(responseCode = "200", description = "The current user was deleted")
     public void delete() {
-        usersService.deleteCurrent();
+        user.delete();
+    }
+
+    @PermissionChecker("user-delete")
+    boolean checkCanDelete(SecurityIdentity securityIdentity) {
+        return user.getRoles().contains(ApelleUserRole.ADMIN) || isCurrent(securityIdentity);
+    }
+
+    private boolean isCurrent(SecurityIdentity securityIdentity) {
+        return user.getId() == usersService.getCurrent().getId();
     }
 }
--- src/main/java/io/github/zannabianca1997/apelle/users/resources/UsersResource.java
+++ src/main/java/io/github/zannabianca1997/apelle/users/resources/UsersResource.java
@@ -1,5 +1,7 @@
 package io.github.zannabianca1997.apelle.users.resources;
 
+import java.util.UUID;
+
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
@@ -10,6 +12,8 @@ import org.jboss.resteasy.reactive.RestResponse;
 import io.github.zannabianca1997.apelle.users.dtos.UserCreateDto;
 import io.github.zannabianca1997.apelle.users.dtos.UserQueryDto;
 import io.github.zannabianca1997.apelle.users.exceptions.UserAlreadyExistsException;
+import io.github.zannabianca1997.apelle.users.exceptions.UserNotFoundByIdException;
+import io.github.zannabianca1997.apelle.users.exceptions.UserNotFoundByNameException;
 import io.github.zannabianca1997.apelle.users.mappers.UserMapper;
 import io.github.zannabianca1997.apelle.users.models.ApelleUser;
 import io.github.zannabianca1997.apelle.users.services.UsersService;
@@ -28,6 +32,8 @@ public class UsersResource {
     UserMapper userMapper;
     @Inject
     UsersService usersService;
+    @Inject
+    UserResource userResource;
 
     @POST
     @PermitAll
@@ -40,5 +46,20 @@ public class UsersResource {
         ApelleUser user = userMapper.createUser(userCreateDto);
         usersService.signup(user);
         return RestResponse.<UserQueryDto>status(Status.CREATED, userMapper.toDto(user));
+    }
+
+    @Path("/me")
+    public UserResource me() {
+        return userResource.of(usersService.getCurrent());
+    }
+
+    @Path("/n/{userName}")
+    public UserResource byName(String userName) throws UserNotFoundByNameException {
+        return userResource.of(usersService.get(userName));
+    }
+
+    @Path("/i/{userId}")
+    public UserResource byId(UUID userId) throws UserNotFoundByIdException {
+        return userResource.of(usersService.get(userId));
     }
 }
