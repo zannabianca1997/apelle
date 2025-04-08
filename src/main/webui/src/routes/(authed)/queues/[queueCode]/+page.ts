ce057a1d356c9bb1bc0aaf3aa3c3ad1753c43afd
zannabianca1997 <zannabianca199712@gmail.com>
2025-04-08 17:04:47 +0200

    Docs: added doc comment

--- src/main/webui/src/routes/(authed)/queues/[queueCode]/+page.ts
+++ src/main/webui/src/routes/(authed)/queues/[queueCode]/+page.ts
@@ -4,6 +4,11 @@ import type { PageLoad } from './$types';
 import { error } from '@sveltejs/kit';
 import { goto } from '$app/navigation';
 
+/**
+ * This route does not contain an actual page, it is just search the queue by code and redirect to the queue page by id.
+ * 
+ * This is by design so urls copied from the web browser will continue to refer to the same queue, even if the queue code changes.
+ */
 export const load: PageLoad = async ({ params: { queueCode } }) => {
     let id: Uuid;
     try {
