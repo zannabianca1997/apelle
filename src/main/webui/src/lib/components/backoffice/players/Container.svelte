d7f451ae76124acbc9bfe117986006cd6aefcd40
zannabianca1997 <zannabianca199712@gmail.com>
2025-04-10 00:02:40 +0200

    Feat: mock of player

---
+++ src/main/webui/src/lib/components/backoffice/players/Container.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import type { CurrentSongQueryDto } from '$lib/apis/apelle';
+	import { _ } from 'svelte-i18n';
+	import Player from './Player.svelte';
+
+	const { current }: { current?: CurrentSongQueryDto } = $props();
+</script>
+
+<section>
+	{#if current}
+		<Player {current} />
+	{:else}
+		<h1>{$_('backoffice.currentSong.nothingPlaying')}</h1>
+	{/if}
+</section>
+
+<style lang="scss">
+	section {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+		gap: 24px;
+
+		width: 100%;
+		height: 161px;
+		border-radius: 6px;
+		padding: 12px;
+
+		background: linear-gradient(90deg, rgba(55, 155, 70, 0.75) 0%, rgba(36, 101, 46, 0.75) 81.67%);
+
+		h1 {
+			margin: 0;
+		}
+	}
+</style>
---
+++ src/main/webui/src/lib/components/backoffice/players/Player.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import type { CurrentSongQueryDto } from '$lib/apis/apelle';
+	import { onMount } from 'svelte';
+
+	const { current }: { current: CurrentSongQueryDto } = $props();
+	let position = $state(current?.position);
+
+	onMount(() => {
+		let interval = setInterval(() => {
+			//TODO
+		}, 1000);
+		return () => clearInterval(interval);
+	});
+</script>
+
+<div class="iframePlaceholder"></div>
+<div class="card">
+	<h2>{current.name}</h2>
+	<h3>{position}</h3>
+</div>
+
+<style lang="scss">
+	.iframePlaceholder {
+		width: 244px;
+		height: 137px;
+
+		background-color: #d6d6d6;
+	}
+</style>
