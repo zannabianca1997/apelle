5e5bf3db561bbdd9ede242974ba02d87bd8cf4b4
zannabianca1997 <zannabianca199712@gmail.com>
2025-04-10 16:58:46 +0200

    Chore: cleanup of directory structure

--- src/main/webui/src/lib/i18n/index.ts
+++ src/main/webui/src/lib/i18n/index.ts
@@ -3,8 +3,8 @@ import { init, register } from 'svelte-i18n'
 
 const defaultLocale = 'en'
 
-register('en', () => import('./locales/en.json'))
-register('it', () => import('./locales/it.json'))
+register('en', () => import('./en.json'))
+register('it', () => import('./it.json'))
 
 init({
     fallbackLocale: defaultLocale,
