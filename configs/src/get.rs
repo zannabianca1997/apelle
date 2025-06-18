c34dde0706f6af2fb1549f0290c422e066f9a9f0
zannabianca1997 <zannabianca199712@gmail.com>
2025-06-18 10:18:18 +0200

    Feat: cache header on the config

--- configs/src/get.rs
+++ configs/src/get.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, time::Duration};
 
 use apelle_common::common_errors::{SQLError, SQLSnafu};
 use apelle_configs_dtos::{QueueConfig, QueueUserAction, QueueUserRole};
@@ -8,6 +8,7 @@ use axum::{
     http::StatusCode,
     response::IntoResponse,
 };
+use axum_extra::{TypedHeader, headers::CacheControl};
 use futures::{FutureExt, StreamExt as _, TryStreamExt};
 use snafu::{OptionExt, ResultExt, Snafu};
 use sqlx::{PgPool, Row};
@@ -36,7 +37,7 @@ impl IntoResponse for GetError {
 pub async fn get(
     Path(id): Path<Uuid>,
     State(db): State<PgPool>,
-) -> Result<Json<QueueConfig>, GetError> {
+) -> Result<(TypedHeader<CacheControl>, Json<QueueConfig>), GetError> {
     let fetch_config = sqlx::query_as(
         unfill!(
             "
@@ -109,14 +110,23 @@ pub async fn get(
         (roles_ids, roles),
     ): ((Uuid, Uuid, Uuid, _, _, _), _) = tokio::try_join!(fetch_config, fetch_roles)?;
 
-    Ok(Json(QueueConfig {
-        id,
-        creator_role: roles_ids.get(&creator_role_id).unwrap().clone(),
-        default_role: roles_ids.get(&default_role_id).unwrap().clone(),
-        banned_role: roles_ids.get(&banned_role_id).unwrap().clone(),
-        roles,
-        autolike,
-        created,
-        updated,
-    }))
+    Ok((
+        // Configs are immutable, as the uuid will change when the config is updated
+        TypedHeader(
+            CacheControl::new()
+                .with_public()
+                .with_max_age(Duration::from_secs(31536000))
+                .with_immutable(),
+        ),
+        Json(QueueConfig {
+            id,
+            creator_role: roles_ids.get(&creator_role_id).unwrap().clone(),
+            default_role: roles_ids.get(&default_role_id).unwrap().clone(),
+            banned_role: roles_ids.get(&banned_role_id).unwrap().clone(),
+            roles,
+            autolike,
+            created,
+            updated,
+        }),
+    ))
 }
