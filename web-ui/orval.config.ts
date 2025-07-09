13c7618a436e97e366d3e1836386cd92e808d2c9
zannabianca1997 <zannabianca199712@gmail.com>
2025-07-09 16:37:47 +0200

    Feat: added orval generation of the ts bindings

---
+++ web-ui/orval.config.ts
@@ -0,0 +1,22 @@
+import { Config, defineConfig } from 'orval';
+import axios from 'axios';
+import type { OpenAPIObject } from 'openapi3-ts/oas30';
+
+const specs = axios.get<OpenAPIObject>('http://localhost:8080/api-docs/openapi.json', {
+    auth: {
+        username: 'admin',
+        password: 'password',
+    }
+}).then(res => res.data);
+
+const config: Promise<Config> = specs.then(target => ({
+    apelle: {
+        input: { target },
+        output: './src/lib/apis/apelle.ts',
+        hooks: {
+            afterAllFilesWrite: 'prettier --write',
+        },
+    },
+}));
+
+export default defineConfig(config);
