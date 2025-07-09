25f8979ea45cdddb3914ca5efa2393c0f72abc37
zannabianca1997 <zannabianca199712@gmail.com>
2025-07-09 22:04:58 +0200

    Fix: removed sass

--- web-ui/vite.config.ts
+++ web-ui/vite.config.ts
@@ -6,17 +6,12 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit(), Icons({ compiler: 'svelte' })],
 	server: {
-		allowedHosts: [
-			'localhost',
-			'front_service'
-		],
-		port: 3000
-	},
-	css: {
-		preprocessorOptions: {
-			scss: {
-				api: 'modern-compiler'
-			}
+		allowedHosts: ['localhost', 'front_service'],
+		port: 3000,
+		hmr: {
+			// Force the use of the 3000 port, that is different from the one
+			// used by the server (reverse proxied by nginx)
+			port: 3000
 		}
 	}
 });
