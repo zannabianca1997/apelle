b144057389439bffd69e7299de3767efedeb2b2a
zannabianca1997 <zannabianca199712@gmail.com>
2025-07-09 22:24:54 +0200

    Feat: global styling

--- web-ui/src/routes/auth/+page.svelte
+++ web-ui/src/routes/auth/+page.svelte
@@ -1,4 +1,16 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
+	import { page } from '$app/state';
+	/* import SigninForm from './SigninForm.svelte';
+	import SignupForm from './SignupForm.svelte'; */
+
+	function onsuccess() {
+		const originalUrl = page.url.searchParams.get('original') ?? '/';
+		goto(originalUrl);
+	}
 </script>
 
-<p>Authentication page</p>
+<main class="flex gap-9">
+	<!-- <SigninForm {onsuccess} />
+	<SignupForm {onsuccess} /> -->
+</main>
