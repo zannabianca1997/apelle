354e5ca2e5ccca8d5884a9e26ebc6a02ba8ca500
zannabianca1997 <zannabianca199712@gmail.com>
2025-07-10 15:45:04 +0200

    WIP: login forms

--- web-ui/src/routes/auth/+page.svelte
+++ web-ui/src/routes/auth/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
+	import { _ } from 'svelte-i18n';
 	import { goto } from '$app/navigation';
 	import { page } from '$app/state';
 	import TextInput from '$lib/components/forms/TextInput.svelte';
 	import { error } from '$lib/errors.svelte';
-	/* import SigninForm from './SigninForm.svelte';
-	import SignupForm from './SignupForm.svelte'; */
+	import Form from './Form.svelte';
 
 	function onsuccess() {
 		const originalUrl = page.url.searchParams.get('original') ?? '/';
@@ -12,7 +12,14 @@
 	}
 </script>
 
-<main class="flex flex-col gap-9">
-	<!-- <SigninForm {onsuccess} />
-	<SignupForm {onsuccess} /> -->
+<main class="flex flex-col gap-9 sm:flex-row">
+	<Form color="red">
+		<TextInput label={$_('login.signinForm.username')} />
+		<TextInput label={$_('login.signinForm.password')} password />
+	</Form>
+	<Form color="blue">
+		<TextInput label={$_('login.signupForm.username')} />
+		<TextInput label={$_('login.signupForm.password')} password />
+		<TextInput label={$_('login.signupForm.passwordCheck')} password />
+	</Form>
 </main>
---
+++ web-ui/src/routes/auth/Form.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import type { HTMLFormAttributes } from 'svelte/elements';
+
+	interface Props extends Omit<HTMLFormAttributes, 'class'> {
+		children?: Snippet;
+		color: 'red' | 'blue';
+	}
+
+	const { children, color, ...formAttributes }: Props = $props();
+</script>
+
+<form
+	{...formAttributes}
+	class={[
+		'm-0.5 flex flex-col items-center justify-start gap-1 rounded-md border-2 p-1.5 transition-all duration-200 ease-in-out hover:m-0 hover:border-3 hover:p-[7px]',
+		color == 'red' ? 'border-redpill' : 'border-bluepill'
+	]}
+>
+	{@render children?.()}
+</form>
