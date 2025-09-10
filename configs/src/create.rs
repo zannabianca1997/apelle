20740f6e97f20aaba76665b7ca472b48e31f5943
zannabianca1997 <zannabianca199712@gmail.com>
2025-09-10 08:42:52 +0200

    Feat: migrated the config creation

--- configs/src/create.rs
+++ configs/src/create.rs
@@ -4,10 +4,8 @@ use apelle_common::db::{SqlError, SqlTx};
 use apelle_configs_dtos::{
     QueueConfig, QueueConfigCreate, QueueUserAction, QueueUserRole, QueueUserRoleCreate,
 };
-use axum::{Json, debug_handler, http::StatusCode, response::IntoResponse};
+use axum::{debug_handler, http::StatusCode, response::IntoResponse, Json};
 use snafu::Snafu;
-use sqlx::types::chrono::{DateTime, FixedOffset};
-use textwrap_macros::unfill;
 use utoipa::{
     IntoResponses,
     openapi::{self, Content, Object, RefOr, Type},
@@ -77,7 +75,7 @@ pub async fn create(
         autolike,
     } = validate(config)?;
 
-    sqlx::query("SET CONSTRAINTS queue_user_role_config_id_is_valid DEFERRED")
+    sqlx::query!("SET CONSTRAINTS queue_user_role_config_id_is_valid DEFERRED")
         .execute(&mut tx)
         .await
         .map_err(SqlError::from)?;
@@ -86,30 +84,26 @@ pub async fn create(
     let config_id = uuid::Uuid::new_v4();
 
     // Insert all roles
-    let (names, max_likes): (Vec<&str>, Vec<i32>) = roles
+    let (names, max_likes): (Vec<String>, Vec<i32>) = roles
         .iter()
-        .map(|(name, QueueUserRoleCreate { max_likes, .. })| (name.as_str(), *max_likes as i32))
+        .map(|(name, QueueUserRoleCreate { max_likes, .. })| (name.clone(), *max_likes as i32))
         .unzip();
-    let role_ids: Vec<Uuid> = sqlx::query_scalar(
-        unfill!(
-            "
-            INSERT INTO queue_user_role (config_id, name, max_likes) 
-            SELECT $1 as config_id, * FROM UNNEST($2, $3) AS t(name, max_likes)
-            RETURNING id
-            "
-        )
-        .trim_ascii(),
+    let role_ids: Vec<Uuid> = sqlx::query_scalar!(
+        "
+        INSERT INTO queue_user_role (config_id, name, max_likes) 
+        SELECT $1 as config_id, * FROM UNNEST($2::text[], $3::int[]) AS t(name, max_likes)
+        RETURNING id
+        ",
+        config_id,
+        &names,
+        &max_likes
     )
-    .bind(config_id)
-    .bind(&names)
-    .bind(max_likes)
     .fetch_all(&mut tx)
     .await
     .map_err(SqlError::from)?;
 
     let name_mapping: HashMap<String, Uuid> = names
         .into_iter()
-        .map(ToOwned::to_owned)
         .zip(role_ids)
         .collect();
 
@@ -123,17 +117,14 @@ pub async fn create(
         })
         .unzip();
 
-    sqlx::query(
-        unfill!(
-            "
-            INSERT INTO queue_user_role_permission (role_id, permission) 
-            SELECT * FROM UNNEST($1, $2) AS t(role_id, permission)
-            "
-        )
-        .trim_ascii(),
+    sqlx::query!(
+        "
+        INSERT INTO queue_user_role_permission (role_id, permission) 
+        SELECT * FROM UNNEST($1::uuid[], $2::queue_user_action[]) AS t(role_id, permission)
+        ",
+        &role_ids,
+        &permissions as &[QueueUserAction]
     )
-    .bind(role_ids)
-    .bind(permissions)
     .execute(&mut tx)
     .await
     .map_err(SqlError::from)?;
@@ -148,17 +139,14 @@ pub async fn create(
         })
         .unzip();
 
-    sqlx::query(
-        unfill!(
-            "
-            INSERT INTO queue_user_grant_roles (role_id, granted_role_id) 
-            SELECT * FROM UNNEST($1, $2) AS t(role_id, granted_role_id)
-            "
-        )
-        .trim_ascii(),
+    sqlx::query!(
+        "
+        INSERT INTO queue_user_grant_roles (role_id, granted_role_id) 
+        SELECT * FROM UNNEST($1::uuid[], $2::uuid[]) AS t(role_id, granted_role_id)
+        ",
+        &role_ids,
+        &can_grant
     )
-    .bind(role_ids)
-    .bind(can_grant)
     .execute(&mut tx)
     .await
     .map_err(SqlError::from)?;
@@ -173,37 +161,31 @@ pub async fn create(
         })
         .unzip();
 
-    sqlx::query(
-        unfill!(
-            "
-            INSERT INTO queue_user_remove_roles (role_id, removed_role_id) 
-            SELECT * FROM UNNEST($1, $2) AS t(role_id, removed_role_id)
-            "
-        )
-        .trim_ascii(),
+    sqlx::query!(
+        "
+        INSERT INTO queue_user_remove_roles (role_id, removed_role_id) 
+        SELECT * FROM UNNEST($1::uuid[], $2::uuid[]) AS t(role_id, removed_role_id)
+        ",
+        &role_ids,
+        &can_revoke
     )
-    .bind(role_ids)
-    .bind(can_revoke)
     .execute(&mut tx)
     .await
     .map_err(SqlError::from)?;
 
     // Insert config
-    let (created, updated): (DateTime<FixedOffset>, DateTime<FixedOffset>) = sqlx::query_as(
-        unfill!(
-            "
-            INSERT INTO queue_config (id, creator_role_id, default_role_id, banned_role_id, autolike) 
-            VALUES ($1, $2, $3, $4, $5)
-            RETURNING created, updated
-            "
-        )
-        .trim_ascii(),
+    let inserted = sqlx::query!(
+        "
+        INSERT INTO queue_config (id, creator_role_id, default_role_id, banned_role_id, autolike) 
+        VALUES ($1, $2, $3, $4, $5)
+        RETURNING created, updated
+        ",
+        config_id,
+        (name_mapping[creator_role.as_str()]),
+        (name_mapping[default_role.as_str()]),
+        (name_mapping[banned_role.as_str()]),
+        autolike
     )
-    .bind(config_id)
-    .bind(name_mapping[creator_role.as_str()])
-    .bind(name_mapping[default_role.as_str()])
-    .bind(name_mapping[banned_role.as_str()])
-    .bind(autolike)
     .fetch_one(&mut tx)
     .await
     .map_err(SqlError::from)?;
@@ -212,8 +194,8 @@ pub async fn create(
         StatusCode::CREATED,
         Json(QueueConfig {
             id: config_id,
-            created,
-            updated,
+            created: inserted.created.into(),
+            updated: inserted.updated.into(),
             creator_role,
             default_role,
             banned_role,
