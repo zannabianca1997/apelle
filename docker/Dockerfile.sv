23469075b064cc749cbfcc444c9b051d03fc6f2b
zannabianca1997 <zannabianca199712@gmail.com>
2025-11-05 18:51:21 +0100

    Feat(gateway): building from root, serving front

--- docker/Dockerfile.sv
+++ docker/Dockerfile.sv
@@ -4,14 +4,14 @@
 
 FROM node:24.3-alpine3.21 as base
 WORKDIR /app
-COPY package*.json ./
+COPY web-ui/package*.json ./
 RUN npm ci
 RUN npx svelte-kit sync
 
 FROM base as dev
 EXPOSE 3000
 
-COPY . .
+COPY web-ui .
 
 VOLUME [ "/app/src" ]
 VOLUME [ "/app/static" ]
@@ -20,11 +20,13 @@ ENTRYPOINT ["npm", "run"]
 CMD [ "dev", "--", "--host", "0.0.0.0" ]
 
 FROM base as build
-COPY . .
+COPY web-ui .
 RUN npm run build
 
 FROM nginx:1.29.0-alpine as prod
 COPY --from=build /app/build /usr/share/nginx/html
-COPY nginx.conf /etc/nginx/conf.d/default.conf
+COPY gateway/nginx-prod.conf /etc/nginx/nginx.conf
+COPY gateway/inbound /etc/nginx/inbound
+COPY gateway/outbound /etc/nginx/outbound
 
 EXPOSE 3000
